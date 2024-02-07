7badaf8421e7271bd08a9f7f529f89db3eda5826
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-06 23:45:42 -0300

    Pipeline Build vs Exceution

---
+++ src/main/java/com/rp/section01/Lecture06MonoSupplierRefactoring.java
@@ -0,0 +1,22 @@
+package com.rp.section01;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class Lecture06MonoSupplierRefactoring {
+
+    public static void main(String[] args) {
+        getName();
+        getName().subscribe(item -> System.out.println("RECEIVED: "+item));
+        getName();
+    }
+
+    private static Mono<String> getName(){
+        System.out.println("entered getName method");
+        return Mono.fromSupplier(() -> {
+            System.out.println("Generating name ...");
+            Util.sleepSecond(3);
+            return Util.faker().name().fullName();
+        }).map(String::toUpperCase);
+    }
+}
