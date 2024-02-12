4ef5827ed53442a854eec9d70571b6e7fe847447
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-12 18:48:11 -0300

    Usingo log()

--- src/main/java/com/rp/section02/Lecture05FluxRange.java
+++ src/main/java/com/rp/section02/Lecture05FluxRange.java
@@ -6,8 +6,10 @@ import reactor.core.publisher.Flux;
 public class Lecture05FluxRange {
     public static void main(String[] args) {
         // Flux.range is used to execute some operation in number of times you want
-        Flux.range(1, 10)
+        Flux.range(3, 10)
+                .log()
                 .map(i -> Util.faker().name().fullName())
+                .log()
                 .subscribe(
                         Util.onNext()
                 );
