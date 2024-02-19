82fea9145471ce576ed9b3229755d6c129a3130d
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-19 20:40:04 -0300

    Flux Generate using counter

---
+++ src/main/java/com/rp/section03/Lecture07FluxGenerateCounter.java
@@ -0,0 +1,20 @@
+package com.rp.section03;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lecture07FluxGenerateCounter {
+
+    public static void main(String[] args) {
+
+        Flux.generate(
+                () -> 1,
+                (state, sink) -> {
+                    String country = Util.faker().country().name();
+                    sink.next(country);
+                    if(state >= 10 || country.toLowerCase().equals("canada")) sink.complete();
+            return state + 1;
+        })
+        .subscribe(Util.subscriber());
+    }
+}
