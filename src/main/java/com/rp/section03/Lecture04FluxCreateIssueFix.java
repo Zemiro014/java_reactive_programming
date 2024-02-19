fb9b75ee1116f5a2d63d7a10add22bb68dfe811a
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-19 19:17:12 -0300

    Check if down stream is cancelled

--- src/main/java/com/rp/section03/Lecture04FluxCreateIssueFix.java
+++ src/main/java/com/rp/section03/Lecture04FluxCreateIssueFix.java
@@ -6,14 +6,14 @@ import reactor.core.publisher.Flux;
 public class Lecture04FluxCreateIssueFix {
     public static void main(String[] args) {
         Flux.create(fluxSink -> {
-                    String country;
-                    do {
-                        country = Util.faker().country().name();
-                        System.out.println("Emitted Country: "+country);
-                        fluxSink.next(country);
-                    } while (!country.toLowerCase().equals("angola") && !fluxSink.isCancelled());
-                })
-                .take(3) // This cancels a subscription when the 3rd item is emitted
-                .subscribe(Util.subscriber());
+                String country;
+                do {
+                    country = Util.faker().country().name();
+                    System.out.println("Emitted Country: "+country);
+                    fluxSink.next(country);
+                } while (!country.toLowerCase().equals("angola") && !fluxSink.isCancelled());
+            })
+            .take(3) // This cancels a subscription when the 3rd item is emitted
+            .subscribe(Util.subscriber());
     }
 }
