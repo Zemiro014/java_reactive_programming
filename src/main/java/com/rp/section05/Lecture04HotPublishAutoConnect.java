1590d9edd75b66731e548ae88c9c26b9e005744b
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-22 10:24:44 -0300

    Transform cold publisher to hot publisher using publish() and autoConnect()

---
+++ src/main/java/com/rp/section05/Lecture04HotPublishAutoConnect.java
@@ -0,0 +1,40 @@
+package com.rp.section05;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+public class Lecture04HotPublishAutoConnect {
+    public static void main(String[] args) {
+
+        // Every time a new subscriber make a subscription to publisher "movieStream", the new subscriber will receive the current item which the publisher emitted.
+        Flux<String> movieStream = Flux.fromStream(() -> getMovie())
+                .delayElements(Duration.ofSeconds(2))
+                .publish()// This transform a cold publisher to hot publisher
+                .autoConnect(0); // In this case isn't demand a min subscriber to start emitting item with autoConnect
+
+        Util.sleepSecond(5);
+        movieStream.subscribe(Util.subscriber("sam"));
+
+        Util.sleepSecond(5);
+
+        movieStream.subscribe(Util.subscriber("mike"));
+        System.out.println("Mike is joining");
+
+        Util.sleepSecond(60);
+    }
+    private static Stream<String> getMovie(){
+        System.out.println("Got the movie streaming request ");
+        return Stream.of(
+                "Scene 1",
+                "Scene 2",
+                "Scene 3",
+                "Scene 4",
+                "Scene 5",
+                "Scene 6",
+                "Scene 7"
+        );
+    }
+}
