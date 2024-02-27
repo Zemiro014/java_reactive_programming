ce4f21c6c4750912dac32b5bf27dd23d7af4fda5
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-27 12:24:26 -0300

    overflow strategy Buffer with size

---
+++ src/main/java/com/rp/section07/Lecture05BufferWithSize.java
@@ -0,0 +1,28 @@
+package com.rp.section07;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lecture05BufferWithSize {
+    public static void main(String[] args) {
+
+        System.setProperty("reactor.bufferSize.small", "16"); // Customizing the buffer size. The default value is 256 defined in QUEES property
+        Flux.create(fluxSink -> {
+                    for (int i = 0; i < 201 && !fluxSink.isCancelled(); i++) {
+                        fluxSink.next(i);
+                        System.out.println("Pushed : "+i);
+                    }
+                    fluxSink.complete();
+                })
+                .onBackpressureBuffer(50, o -> System.out.println("DROPPED : "+o))
+                .publishOn(Schedulers.boundedElastic())
+                .doOnNext(i -> {
+                    // This operation will cause the soulfully consume item in subscriber side
+                    Util.sleepMills(10);
+                })
+                .subscribe(Util.subscriber());
+
+        Util.sleepSecond(10);
+    }
+}
