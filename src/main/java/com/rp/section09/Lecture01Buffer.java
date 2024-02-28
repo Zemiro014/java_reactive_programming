2d8c9d899bdd6757e5ed7a9268b1a89d20539b91
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-27 21:14:42 -0300

    Batching with buffer

---
+++ src/main/java/com/rp/section09/Lecture01Buffer.java
@@ -0,0 +1,24 @@
+package com.rp.section09;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lecture01Buffer {
+
+    public static void main(String[] args) {
+
+        eventStream()
+                .buffer(5) // Allow that the subscriber consuming the items in LIST
+                .subscribe(Util.subscriber());
+
+
+        Util.sleepSecond(60);
+    }
+
+    private static Flux<String> eventStream(){
+        return Flux.interval(Duration.ofMillis(300))
+                .map(i -> "event"+i);
+    }
+}
