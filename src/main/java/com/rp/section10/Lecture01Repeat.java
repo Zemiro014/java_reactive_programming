f44c2d092d13626cd6e19dc55036c334647a9ce6
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-02-28 18:43:12 -0300

    using repeat conditionally

--- src/main/java/com/rp/section10/Lecture01Repeat.java
+++ src/main/java/com/rp/section10/Lecture01Repeat.java
@@ -3,12 +3,15 @@ package com.rp.section10;
 import com.rp.courseutil.Util;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lecture01Repeat {
 
+    private static AtomicInteger atomicInteger = new AtomicInteger();
     public static void main(String[] args) {
 
         getIntegers()
-                .repeat(2) // used to repeat the emission of items multiple times, allow the subscriber consuming 2 times
+                .repeat(() -> atomicInteger.get() < 14) // used to repeat the emission of items multiple times, allow the subscriber consuming 2 times
                 .subscribe(Util.subscriber());
 
     }
@@ -16,6 +19,7 @@ public class Lecture01Repeat {
     private static Flux<Integer> getIntegers(){
         return Flux.range(1, 3)
                 .doOnSubscribe(s -> System.out.println("Subscribed"))
-                .doOnComplete(() -> System.out.println("--Completed"));
+                .doOnComplete(() -> System.out.println("--Completed"))
+                .map(i -> atomicInteger.getAndIncrement());
     }
 }
