97357bec3fed4927ad0994dc9891e27cf31d2934
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-03-01 18:08:37 -0300

    Step Verifier - Virtual Time Test 2

--- src/test/java/com/rp/test/Lecture05VirtualTimeTest.java
+++ src/test/java/com/rp/test/Lecture05VirtualTimeTest.java
@@ -19,6 +19,17 @@ public class Lecture05VirtualTimeTest {
                 .expectComplete();
     }
 
+    @Test
+    public void test2(){
+
+        StepVerifier.withVirtualTime(() -> timeConsumingFlux())
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(4)) // Verify if something not happened during some period
+                .thenAwait(Duration.ofSeconds(20))
+                .expectNext("1a", "2a", "3a", "4a")
+                .verifyComplete();
+    }
+
     private Flux<String> timeConsumingFlux(){
         return Flux.range(1, 4)
                 .delayElements(Duration.ofSeconds(5))
